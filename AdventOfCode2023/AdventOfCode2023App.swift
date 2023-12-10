cc3065ae26bd88312627d7747f334d9075ac0d2f
Valery Zazulin <zazulinvalerii@gmail.com>
2023-12-10 17:11:46 +0100

    Initial Commit

---
+++ AdventOfCode2023/AdventOfCode2023App.swift
@@ -0,0 +1,17 @@
+//
+//  AdventOfCode2023App.swift
+//  AdventOfCode2023
+//
+//  Created by Валерий Зазулин on 10.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct AdventOfCode2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AdventOfCode2023/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AdventOfCode2023
+//
+//  Created by Валерий Зазулин on 10.12.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
