1ab2096e8b8b1356b037430e60fb6856a40f8570
Valery Zazulin <zazulinvalerii@gmail.com>
2023-12-10 17:16:49 +0100

    Test commit

---
+++ AdventOfCode2023/1.1_Trebuchet/TrebuchetView.swift
@@ -0,0 +1,18 @@
+//
+//  TrebuchetView.swift
+//  AdventOfCode2023
+//
+//  Created by Валерий Зазулин on 10.12.2023.
+//
+
+import SwiftUI
+
+struct TrebuchetView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrebuchetView()
+}
