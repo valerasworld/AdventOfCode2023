b7071eecce999f76331ff74a67347a3f69dafba3
Valery Zazulin <zazulinvalerii@gmail.com>
2023-12-14 01:46:45 +0100

    Day 6.2 - WaitForIt2 - is solved! (not commented yet)

--- AdventOfCode2023/6.2_WaitForIt2/6.2_WaitForIt2Playground.playground/Contents.swift
+++ AdventOfCode2023/6.2_WaitForIt2/6.2_WaitForIt2Playground.playground/Contents.swift
@@ -22,7 +22,6 @@ class WaitForIt2 {
     
     func amountOfRecords(input: String) -> Int {
         var count: Int = 0
-        //
         
         var inputStrings: [String] = input.components(separatedBy: "\n")
         
@@ -30,6 +29,7 @@ class WaitForIt2 {
         var distance: Int = 0
          
         var numberString: String = ""
+        
         // Time:
         for (charInd, char) in inputStrings[0].enumerated() {
             
@@ -65,44 +65,68 @@ class WaitForIt2 {
                 continue
             }
         }
-        
-        print(distance)
-        
+                
         // Ready to count:
         
         var firstWin: Int = 1
         var lastWin: Int = 1
+
+        let range = 1...time
+        var left = range.lowerBound
+        var right = range.upperBound
         
-        // from the begining
-        for miliseconds in 1...time {
-            let speed = miliseconds * 1
-            let restTime = time - miliseconds
-            let myDistance = restTime * speed
-            if myDistance > distance {
-                firstWin = miliseconds
-                break
-            } else {
-                continue
+        while left <= right {
+            let mid = left + (right - left) / 2
+            let nextToMid = mid + 1
+            let speed = mid * 1
+            let failRestTime = time - mid
+            let winRestTime = time - nextToMid
+            let failDistance = failRestTime * speed
+            let winDistance = winRestTime * (speed + 1)
+            
+            
+            if failDistance <= distance {
+                if distance < winDistance {
+                    firstWin = mid + 1 // !
+                    left = range.lowerBound
+                    right = range.upperBound
+                    break
+                } else {
+                    left = mid + 1
+                }
+            } else if failDistance > distance {
+                right = mid - 1
             }
         }
-        // from the ending
+                
+        left = range.lowerBound
+        right = range.upperBound
         
-        for miliseconds in (1...time).reversed() {
-            let speed = miliseconds * 1
-            let restTime = time - miliseconds
-            let myDistance = restTime * speed
-            if myDistance > distance {
-                lastWin = miliseconds
-                break
-            } else {
-                continue
+        while left <= right {
+            let mid = left + (right - left) / 2
+            let nextToMid = mid + 1
+            let speed = mid * 1
+            let winRestTime = time - mid
+            let failRestTime = time - nextToMid
+            let winDistance = winRestTime * speed
+            let failDistance = failRestTime * (speed + 1)
+
+            
+            if winDistance > distance {
+                if distance >= failDistance {
+                    lastWin = mid // !
+                    break
+                } else {
+                    left = mid + 1
+                }
+            } else if winDistance <= distance {
+               right = mid - 1
             }
         }
-        
-        // product
+                
+        // count
         count = lastWin - firstWin + 1
         
-        //
         return count
     }
 }
